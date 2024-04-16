72f1ce64bfb30545d7f800ac353b52b59218aad3
G-MW <139481195+G-MW@users.noreply.github.com>
2024-04-16 19:22:48 +0200

    ref tools (#3)

--- verner-git/src/cli.rs
+++ verner-git/src/cli.rs
@@ -11,8 +11,11 @@ pub struct Args
     #[arg(long = "local", default_value_t = false)]
     pub use_local: bool,
 
-    #[arg(long = "branch", short = 'b', default_value = None)]
-    pub branch_name: Option<String>,
+    #[arg(long = "use-ref", default_value = None)]
+    pub use_ref: Option<String>,
+
+    #[arg(long = "on-branch", default_value = None)]
+    pub override_branch_name: Option<String>,
 
     #[arg(long = "git-dir", default_value = None)]
     pub git_dir: Option<PathBuf>
